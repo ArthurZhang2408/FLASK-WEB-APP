91c1a99f87fbe2e8206af44b664a01e1cbcb6923
Arthur Zhang <arthurzhang2408@hotmail.com>
2022-07-11 21:51:35 -0700

    add change profile system

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, flash, render_template, request, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, User
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 import json
 
@@ -22,6 +23,44 @@ def home():
             flash('Note added!', category='success')
 
     return render_template("home.html", user=current_user)
+
+@views.route('/message', methods=['GET', 'POST'])
+@login_required
+def message():
+    return render_template("message.html", user=current_user)
+
+@views.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    if request.method == 'POST':
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
+        password = request.form.get('password')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        if first_name != current_user.first_name:
+            if len(first_name) < 2:
+                flash('First name is not valid.', category='error')
+            else:
+                User.query.get(current_user.id).first_name = first_name
+                flash('First name updated!', category='success')
+        if last_name != current_user.last_name:
+            if len(last_name) < 2:
+                flash('Last name is not valid.', category='error')
+            else:
+                User.query.get(current_user.id).last_name = last_name
+                flash('Last name updated!', category='success')
+        if check_password_hash(current_user.password, password) and password != password1:
+            if len(password1) < 7:
+                flash('Password must has at least 7 characters.', category='error')
+            elif password1 != password2:
+                flash('Passwords don\'t match.', category='error')
+            else:
+                User.query.get(current_user.id).password = generate_password_hash(password1, method='sha256')
+                flash('Password updated!', category='success')
+        db.session.commit()
+
+    return render_template("profile.html", user=current_user)
     
 @views.route('/delete-note', methods=['POST'])
 def delete_note():
